b8abdaa7e4688264cf71c68f7aa5a03ab0074eb0
IHIutch <jbhutch01@gmail.com>
2020-04-04 14:39:30 -0400

    Updates

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,33 +1,31 @@
 <template>
   <Layout>
-
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
+    <h1>Hey</h1>
   </Layout>
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: 'Hello, world!'
-  }
-}
+    title: "Hello, world!",
+  },
+  data() {
+    return {
+      apiUrl:
+        "https://services1.arcgis.com/CgOSc11uky3egK6O/arcgis/rest/services/ErieCounty_Daily_Totals/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Date%20asc&resultOffset=0&resultRecordCount=2000&cacheHint=true",
+      jsonData: {},
+    };
+  },
+  created() {
+    fetch(this.apiUrl)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.jsonData = data.features.map((data) => {
+          return data.attributes;
+        });
+      });
+  },
+};
 </script>
-
-<style>
-.home-links a {
-  margin-right: 1rem;
-}
-</style>
