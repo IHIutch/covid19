4154f760bb5d3e9788a5ac5970848da078161624
IHIutch <jbhutch01@gmail.com>
2020-04-04 14:43:38 -0400

    install tailwind, postcss utils

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,10 +1,24 @@
-// This is where project configuration and plugin options are located. 
+// This is where project configuration and plugin options are located.
 // Learn more: https://gridsome.org/docs/config
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
+const tailwind = require("tailwindcss");
+const autoprefixer = require("autoprefixer");
+const purgecss = require("@fullhuman/postcss-purgecss");
+
+const postcssPlugins = [tailwind, autoprefixer];
+
+if (process.env.NODE_ENV === "production") postcssPlugins.push(purgecss());
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
-}
+  siteName: "Gridsome",
+  plugins: [],
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: postcssPlugins,
+      },
+    },
+  },
+};
